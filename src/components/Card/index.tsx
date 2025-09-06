006997922f09ec1cf9b7cce33bd62baf7a0bd66e
Senacmun <senamunsenac@gmail.com>
2025-09-06 02:15:37 +0000

    commit

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,18 +10,20 @@ interface CardProps {
 
 export default function Card({ title, subtitle, href, icon }: CardProps) {
   return (
-    <Link href={href}>
-      <div className="bg-white rounded-xl overflow-hidden shadow-xl border border-yellow-custom">
-        <div className="p-6 flex items-center gap-2 text-light-blue-custom">
-          <div className="text-5xl">{icon}</div>
-          <div className="">
-            <h2 className="font-semibold text-lg text-yellow-custom">
-              {title}
-            </h2>
-            <p className="text-black">{subtitle}</p>
+    <Link href={href} legacyBehavior>
+      <a className="block h-full">
+        <div className="h-full bg-white rounded-xl overflow-hidden shadow-xl border border-yellow-custom transition-all duration-300 hover:shadow-2xl hover:-translate-y-1">
+          <div className="p-6 flex items-center gap-2 text-light-blue-custom">
+            <div className="text-5xl">{icon}</div>
+            <div>
+              <h2 className="font-semibold text-lg text-yellow-custom">
+                {title}
+              </h2>
+              <p className="text-black">{subtitle}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </Link>
   );
 }
